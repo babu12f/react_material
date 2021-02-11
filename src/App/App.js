1657b1e9caad8ef1f13111a150a9ead1aec15584
Babor <babu_12f@yahoo.com>
2021-02-11 21:08:46 +0600

    material theme customize

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,39 @@
 import React from 'react'
 import './App.css'
 import SideMenu from '../components/SideMenu'
-import { CssBaseline, makeStyles } from '@material-ui/core'
+import { createMuiTheme, CssBaseline, makeStyles, ThemeProvider } from '@material-ui/core'
 import Header from '../components/Header'
+import PageHeader from '../components/PageHeader'
+import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline'
+
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: "#333996",
+			light: '#3c44b126'
+		},
+		secondary: {
+			main: "#f83245",
+			light: '#f8324526'
+		},
+		background: {
+			default: "#f4f5fd"
+		},
+	},
+	overrides: {
+		MuiAppBar: {
+			root: {
+				transform: 'translateZ(0)'
+			}
+		}
+	},
+	props: {
+		MuiIconButton: {
+			disableRipple: true
+		}
+	}
+})
 
 const useStyles = makeStyles({
 	appMain: {
@@ -16,13 +47,17 @@ function App() {
 	const classes = useStyles()
 
 	return (
-		<>
+		<ThemeProvider theme={theme}>
 			<SideMenu />
 			<div className={classes.appMain}>
 				<Header />
+				<PageHeader
+				title="Page Header"
+				subTitle="Page Description"
+				icon={<PeopleOutlineIcon fontSize="large" />} />
 			</div>
 			<CssBaseline />
-		</>
+		</ThemeProvider>
 	)
 }
 
